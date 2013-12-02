21a125856f01678d82c2a92af79e9c2024c841bc
pigram86 <todd@toddpigram.com>
2013-12-02 12:47:04 -0500

    attr added

---
+++ metadata.rb
@@ -0,0 +1,10 @@
+name             'w2k8_default'
+maintainer       'Todd Pigram'
+maintainer_email 'todd@toddpigram.com'
+license          'Apache 2.0'
+description      'Installs/Configures w2k8_default'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '1.1.0'
+depends          "windows"
+depends          "powershell"
+
---
+++ recipes/default.rb
@@ -0,0 +1,38 @@
+#
+# Cookbook Name:: w2k8_default
+# Recipe:: default
+#
+# Copyright (C) 2013 Todd Pigram
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#    http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# Powershell Default Server
+powershell node[:w2k8][:default] do
+  code <<-EOH
+  Import-Module ServerManager
+  Add-WindowsFeature FS-FileServer
+  Add-WindowsFeature Backup
+  Add-WindowsFeature Backup-Tools
+  Add-WindowsFeature Net-Framework-Core
+  Add-WindowsFeature Powershell-ISE
+  Add-WindowsFeature WSRM
+  Add-WindowsFeature GPMC
+  EOH
+  not_if {reboot_pending?}
+end
+
+# if feature installs, schedule a reboot at end of chef run
+windows_reboot 60 do
+  reason 'reboot needed'
+  only_if {reboot_pending?}
+end 
